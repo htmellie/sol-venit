30d8ef4c9c73e85a7301bc859361f7d22f03a2ae
htmellie <ellieredmayne@hotmail.com>
2022-01-23 15:34:22 +0000

    API call

--- src/Components/Fetch/index.js
+++ src/Components/Fetch/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-
-const lat = 0;
-const lng = 0;
-
-async function getSomeStats() {
-  const response = await fetch(
-    `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`
-  );
-}
+import React, { useEffect } from "react";
 
 function Fetch() {
+  const lat = 52.4862;
+  const lng = 1.8904;
+
+  useEffect(() => {
+    async function getSomeStats() {
+      const response = await fetch(
+        `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=2022-02-27`
+      );
+      const data = await response.json();
+      console.log(data);
+    }
+    getSomeStats();
+  }, []);
+
   return (
     <div>
       <h1>hello</h1>
