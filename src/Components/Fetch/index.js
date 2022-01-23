edfcc602ba90725b9cf3dd45581d85d8987c1bea
Nathan Hinks <nathanhinks.dev@gmail.com>
2022-01-23 15:50:29 +0000

    add sunrise state

--- src/Components/Fetch/index.js
+++ src/Components/Fetch/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from 'react';
 
 function Fetch() {
+  const [sunrise, setSunrise] = useState('');
   const lat = 52.4862;
   const lng = 1.8904;
 
@@ -10,6 +11,9 @@ function Fetch() {
         `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=2022-02-27`
       );
       const data = await response.json();
+
+      setSunrise(data.results.sunrise);
+      console.log(h1text);
       console.log(data);
     }
     getSomeStats();
@@ -17,7 +21,7 @@ function Fetch() {
 
   return (
     <div>
-      <h1>hello</h1>
+      <h1>the sun will rise in Birmingham at {sunrise} today</h1>
     </div>
   );
 }
